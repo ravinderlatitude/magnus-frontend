e7263153fa4747450300905396175cfaed2bbf2f
Prahladkumar Dabhi <prahlad.d@latitudetechnolabs.com>
2023-04-11 11:18:13 +0530

    add axios and create testimonial componenet. Add testimonial componenet in home, faq and about us page

--- src/pages/aboutus/index.js
+++ src/pages/aboutus/index.js
@@ -6,27 +6,12 @@ import AboutImg from "../../assets/images/AboutImg.png";
 import Banner from "../../components/banner";
 import ModalRegister from "../../components/ModalRegister";
 
-import ICLearning from "../../assets/images/ICLearning.svg";
-import ICStream from "../../assets/images/ICStream.svg";
-
 import ICSetUp from "../../assets/images/ICSetUp.svg";
 import ICVarify from "../../assets/images/ICVarify.svg";
 import ICChoose from "../../assets/images/ICChoose.svg";
 import ICPay from "../../assets/images/ICPay.svg";
 
-import UserImg from "../../assets/images/UserImg.png";
-import StarFill from "../../assets/images/star-fill.svg";
-import StarBlank from "../../assets/images/star-blank.svg";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/autoplay";
+import Testimonials from "@/components/Testimonials";
 
 // import LOGO from "../assets/images/logo.svg";
 export default function aboutus() {
@@ -135,131 +120,7 @@ export default function aboutus() {
             <div className="title-text text-center">
               <h2>Testimonials</h2>
             </div>
-            <Swiper
-              // install Swiper modules
-              modules={[Navigation, Pagination, Autoplay]}
-              spaceBetween={40}
-              slidesPerView={1}
-              loop
-              autoplay={{ delay: 4000 }}
-              // navigation
-              // pagination={{ clickable: true }}
-              breakpoints={{
-                // when window width is >= 576
-                576: {
-                  // width: 576,
-                  slidesPerView: 2,
-                },
-              }}
-            >
-              <SwiperSlide>
-                <div className="testi-blcok">
-                  <div className="user-info">
-                    <div className="user-img">
-                      <Image alt="" src={UserImg} />
-                    </div>
-                    <div>
-                      <h4>Friskidia</h4>
-                      <h6>Client</h6>
-                    </div>
-                  </div>
-                  <div className="ratings">
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarBlank} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarBlank} />
-                    </span>
-                  </div>
-                  <p className="mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec rutrum dapibus luctus.
-                  </p>
-                  <span className="top-right-corner"> </span>
-                </div>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <div className="testi-blcok">
-                  <div className="user-info">
-                    <div className="user-img">
-                      <Image alt="" src={UserImg} />
-                    </div>
-                    <div>
-                      <h4>Friskidia</h4>
-                      <h6>Client</h6>
-                    </div>
-                  </div>
-                  <div className="ratings">
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarBlank} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarBlank} />
-                    </span>
-                  </div>
-                  <p className="mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec rutrum dapibus luctus.
-                  </p>
-                  <span className="top-right-corner"> </span>
-                </div>
-              </SwiperSlide>
-
-              <SwiperSlide>
-                <div className="testi-blcok">
-                  <div className="user-info">
-                    <div className="user-img">
-                      <Image alt="" src={UserImg} />
-                    </div>
-                    <div>
-                      <h4>Friskidia</h4>
-                      <h6>Client</h6>
-                    </div>
-                  </div>
-                  <div className="ratings">
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarFill} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarBlank} />
-                    </span>
-                    <span>
-                      <Image alt="" src={StarBlank} />
-                    </span>
-                  </div>
-                  <p className="mt-3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec rutrum dapibus luctus.
-                  </p>
-                  <span className="top-right-corner"> </span>
-                </div>
-              </SwiperSlide>
-            </Swiper>
+            <Testimonials />
           </div>
         </div>
       </div>
